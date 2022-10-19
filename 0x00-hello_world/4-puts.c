268ae49bcdbfe634d670574697a7c3c3a4c87598
kenley-grafix <emmanuelkenley244@gmail.com>
2022-10-18 21:54:06 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ * Using puts function
+ * return is always 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilinqual puzzle");
+	puts("\"Programming is like building a multilinqual puzzle");
 	return (0);
 }
